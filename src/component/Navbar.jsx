6953c97cd4e3b4461bbeaf9ad80ea0c35ea0c404
Shekharpawar1 <86783159+Shekharpawar1@users.noreply.github.com>
2022-06-09 15:15:41 +0530

    added navbar component

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="nav">
+      <div className="nav__logo">
+        <img src="./assets/logo.png" alt="designo logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
